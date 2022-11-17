f1cc9375722eea521760b5b5c3f6de5e384266c7
yeahright18 <szymonsk312@gmail.com>
2022-11-17 15:20:50 +0100

    commit2

--- diary.rb
+++ diary.rb
@@ -3,9 +3,7 @@ class Diary
   attr_reader :students, :subjects, :grades, :start_id
   
   def initialize(name)
- 
     @name = name
-    
     @students = []
     @subjects = [
       Subject.new('Polski'), 
@@ -13,88 +11,65 @@ class Diary
       Subject.new('Informatyka'),       
       Subject.new('Matematyka')
     ]
-
     @start_id = 1
-
     @grades = {}
-
   end
  
   def student_grades(id)
-  
     id = id.to_i
     grades[id].each {|key, value| puts "#{key} ==b #{value}"}
-  
   end
 
   def student_grades_by_name(full_name) 
-
     full_name = students.find {|student| student.full_name == full_name}
     student_grades(full_name.id)
-    
   end
 
  
   def subject_grades(subject_name)
-  
     grades.each do |id, value|
       id = @students.find{|student| student.id == id}
       value = value[subject_name]
       puts "#{id.id}. #{id.full_name}-> #{value || []}"  
     end
-   
   end
  
   def show_students
-   
     students.each { |student| puts "#{student.id}. #{student.name} #{student.last_name}" }
-    
   end
  
   def show_subjects
-    
     subjects.sort_by(&:name).each { |subject| puts subject.name }
-    
   end
  
   def add_student(first_name, last_name)
-    
     students << Student.new(first_name, last_name, @start_id)
     @start_id += 1
-    
   end
  
   def remove_student(id)
     students.delete_if do |student| 
       student.id == id 
     end
-    
   end
  
   def add_grade_by_id(subject_name, student_id, grade)
-
      grades[student_id] ||= {}
      puts grades[student_id][subject_name] ||= [] 
      grades[student_id][subject_name].push(grade)
-     
   end
  
   def add_grade_by_full_name(subject_name, student_name, grade)
-    
      student = students.find {|student| student.full_name == student_name}
      add_grade_by_id(subject_name, student.id, grade)
-
   end
-
 end
  
 class Subject
   attr_reader :name
- 
   def initialize(name)
     @name = name
   end
-  
 end
  
 class Student
@@ -104,13 +79,10 @@ class Student
     @last_name = last_name
     @id = id
   end
-
   def full_name
     "#{name} #{last_name}"
   end
-
 end
- 
  
 # diary = Diary.new("III klasa")
 # diary.add_student('Jan', 'Kowalski')
